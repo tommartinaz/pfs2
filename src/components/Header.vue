9880d2e624e7147944d9a3a8e303f9b2aff0194a
Tom Martin <tom@Poe.local>
2019-02-26 13:51:05 -0700

    added login to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,62 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class='collapse navbar-collapse'>
-        <a class="navbar-brand" href="/">PFS Character Tracker</a>
-        <ul class="navbar-nav">
-            <router-link :to="{ name: 'homeLink' }" tag='li' class="nav-link">Home</router-link>
-            <router-link :to="{ name: 'playersLink'}" tag="li" class="nav-link">Players</router-link>
-            <router-link :to="{ name: 'characterListLink' }" tag='li' class="nav-link">Characters</router-link>
-            <router-link :to="{ name: 'scenariosLink' }" tag='li' class="nav-link">Scenarios</router-link>
-        </ul>
-        </div>
-    </nav>
+    <b-navbar toggleable="md" type="light" variant="info">
+
+        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+        <b-navbar-brand href="/">PFS Character Tracker</b-navbar-brand>
+
+        <b-collapse is-nav id="nav_collapse">
+
+            <b-navbar-nav>
+                <b-nav-item :to="{ name: 'homeLink' }" router-tag="li">Home</b-nav-item>
+                <b-nav-item :to="{ name: 'characterListLink' }" router-tag="li">Characters</b-nav-item>
+                <b-nav-item :to="{ name: 'scenarioListLink' }" router-tag="li">Scenarios</b-nav-item>
+            </b-navbar-nav>
+
+            <!-- Right aligned nav items -->
+            <b-navbar-nav class="ml-auto">
+
+                <b-nav-item right v-b-modal.modalLogin v-if="!isLoggedIn">Login</b-nav-item>
+                <b-nav-item right v-else @click="logout">Logout</b-nav-item>
+
+            </b-navbar-nav>
+
+        </b-collapse>
+        <b-modal id="modalLogin" title="Player Login" @ok="login">
+            <div class="login-form">
+                <input v-model="email" type="email" placeholder="email" />
+                <input v-model="password" type="password" placeholder="password" />
+                <p style="color:red; font-size:30px">{{error}}</p>
+            </div>
+        </b-modal>
+    </b-navbar>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+    name: "Header",
+    data() {
+        return {
+            email: '',
+            password: '',
+            error: '',
+            player_id: window.localStorage.getItem('player_id')
+        }
+    },
+    methods: {
+        logout() {
+            this.$store.dispatch('logoutPlayer');
+        },
+        login() {
+            this.player_id = this.$store.dispatch('loginPlayer', {email: this.email, password: this.password})
+        },
+    },
+    computed: {
+        ...mapGetters(['isLoggedIn'])
+    }
+}
+</script>
 
 <style scoped>
 
